e779d7ca366589e3c9e0019aa6900ca08f902bd2
Subhajit Sahu <wolfram77@gmail.com>
2020-08-28 11:43:33 +0000

    something to read

--- Philosopher.java
+++ Philosopher.java
@@ -1,15 +1,42 @@
 import java.util.concurrent.*;
 
+// There are 5 philosophers sitting around a
+// table. Each philosopher thinks, or when hungry
+// eats. Each philosopher has a bowl of noodles,
+// but there are only 5 forks between each
+// philosopher. However, in order to eat, a
+// philosopher needs both forks. A deadlock occurs
+// when all philosophers pick up one fork, but are
+// waiting for the other fork to be put down.
+
+// One idea to solve the dependency cycle is to
+// alter the order in which the last philosopher
+// picks up forks.
+
 class Philosopher extends Thread {
   int i;
   static int N = 5;
   static Semaphore[] fork;
   static Philosopher[] philosopher;
+  // i: each philosopher's id
+  // N: number of philosophers
+  // fork: N forks placed between them
+  // philosopher: all N philosophers
 
   Philosopher(int id) {
     i = id;
   }
 
+  // Each philosoper:
+  // 1. thinks (sleep 100ms)
+  // 2. picks left fork (i) (or waits)
+  // 3. picks right fork (i+1) (or waits)
+  // 4. eats (sleep 100ms)
+  // 5. puts back right fork
+  // 6. puts back left fork
+  // EXCEPT the last philospher, who instead
+  // picks up his right fork first; this is
+  // what helps us to avoid deadlock.
   public void run() {
     try {
       while(true) {
@@ -31,6 +58,9 @@ class Philosopher extends Thread {
     }
   }
 
+  // 1. forks are placed
+  // 2. philosophers are seated
+  // 3. all philosophers now get to work
   public static void main(String[] args) {
     fork = new Semaphore[N];
     philosopher = new Philosopher[N];
